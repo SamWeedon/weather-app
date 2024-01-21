dd7d150d8ef0e5220d2d5b18b0bd4f3ae90b0c9f
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2024-01-20 19:01:54 -0700

    Change fetch request to get forecast data

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const fetchWeatherInfo = async function (location) {
+  const processedWeatherObject = {};
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=99d1c58c2d6a4023bff11303241901&q=${location}`,
+    `https://api.weatherapi.com/v1/forecast.json?key=99d1c58c2d6a4023bff11303241901&days=3&q=${location}`,
     { mode: "cors" }
   );
-  console.log(response);
-  const weatherInfo = await response.json();
-  console.log(weatherInfo);
+
+  const weatherInfoObject = await response.json();
+  console.log(weatherInfoObject);
+  /*
+  processedWeatherObject.temp_f = weatherInfoObject.current.temp_f;
+  processedWeatherObject.temp_c = weatherInfoObject.current.temp_c;
+
+  console.log(processedWeatherObject);
+  */
+  return processedWeatherObject;
 };
 
 fetchWeatherInfo("New York City");
+/*
+const processWeatherInfo = function (weatherInfoObject) {
+  const tempF = weatherInfoObject.current.temp_f;
+  console.log(tempF);
+};
+*/
+
+//processWeatherInfo(fetchWeatherInfo("New York City"));
