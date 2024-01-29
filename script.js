56ff3a830fff0184c9f08adb5e96859094e861d7
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2024-01-28 17:42:33 -0700

    Clean up and comment code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const fetchWeatherInfo = async function (location) {
-  const processedWeatherObject = {};
+  /*
+  Fetch weather info object from weatherapi.com
+  */
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=99d1c58c2d6a4023bff11303241901&days=3&q=${location}`,
     { mode: "cors" }
@@ -7,12 +9,10 @@ const fetchWeatherInfo = async function (location) {
 
   const weatherInfoObject = await response.json();
 
-  processedWeatherObject.forecastdays = weatherInfoObject.forecast.forecastday;
-  processedWeatherObject.location = weatherInfoObject.location;
-  processedWeatherObject.current = weatherInfoObject.current;
-  return processedWeatherObject;
+  return weatherInfoObject;
 };
 
+// get DOM elements
 const searchBar = document.getElementById("search-bar");
 const searchButton = document.getElementById("search-button");
 const selectedWeather = document.getElementById("selected-weather");
@@ -31,32 +31,11 @@ const afterTomorrowCondition = document.getElementById(
 const afterTomorrowLow = document.getElementById("day-after-tomorrow-low");
 const afterTomorrowHigh = document.getElementById("day-after-tomorrow-high");
 
-let weatherObject;
-searchButton.addEventListener("click", (e) => loadInfo(e));
-
-for (let i = 0; i < hourButtons.length; i++) {
-  hourButtons[i].addEventListener("mouseover", (e) => {
-    selectedTime.textContent = e.target.id;
-    selectedConditions.src =
-      weatherObject.forecastdays[0].hour[i].condition.icon;
-    selectedTemperature.textContent = `${weatherObject.forecastdays[0].hour[i].temp_f} F`;
-  });
-}
-
-const loadInfo = async function (event) {
-  if (event) {
-    event.preventDefault();
-  }
-  weatherObject = await fetchWeatherInfo(searchBar.value);
-  searchBar.value = `${weatherObject.location.name}, ${
-    weatherObject.location.region
-      ? weatherObject.location.region
-      : weatherObject.location.country
-  }`;
-  selectedTime.textContent = "Now";
-  selectedConditions.src = weatherObject.current.condition.icon;
-  selectedTemperature.textContent = `${weatherObject.current.temp_f} F`;
-
+const getWeekdayName = function (weekdayIndex) {
+  /*
+  Given an index from 0-6, like from the DateObject.getDay() method,
+  return its corresponding weekday
+  */
   const weekdays = [
     "Sunday",
     "Monday",
@@ -66,18 +45,70 @@ const loadInfo = async function (event) {
     "Friday",
     "Saturday",
   ];
-  const tomorrow = new Date(`${weatherObject.forecastdays[1].date}T00:00`);
-  tomorrowDate.textContent = weekdays[tomorrow.getDay()];
-  tomorrowCondition.src = weatherObject.forecastdays[1].day.condition.icon;
-  tomorrowLow.textContent = `Low: ${weatherObject.forecastdays[1].day.mintemp_f} F`;
-  tomorrowHigh.textContent = `High: ${weatherObject.forecastdays[1].day.maxtemp_f} F`;
-
-  const afterTomorrow = new Date(`${weatherObject.forecastdays[2].date}T00:00`);
-  afterTomorrowDate.textContent = weekdays[afterTomorrow.getDay()];
-  afterTomorrowCondition.src = weatherObject.forecastdays[2].day.condition.icon;
-  afterTomorrowLow.textContent = `Low: ${weatherObject.forecastdays[2].day.mintemp_f} F`;
-  afterTomorrowHigh.textContent = `High: ${weatherObject.forecastdays[2].day.maxtemp_f} F`;
+  return weekdays[weekdayIndex];
 };
 
-searchBar.value = "Baltimore";
-loadInfo();
+let weatherObject;
+const loadWeather = async function (event) {
+  /*
+  Load and display all weather info
+  */
+
+  // prevent form "submission" on search button click, while still allowing function to be called
+  // without an event argument
+  if (event) {
+    event.preventDefault();
+  }
+
+  // get weather info object from API
+  weatherObject = await fetchWeatherInfo(searchBar.value);
+
+  // Standardize and specify searchbar value
+  searchBar.value = `${weatherObject.location.name}, ${
+    weatherObject.location.region
+      ? weatherObject.location.region
+      : weatherObject.location.country
+  }`;
+
+  // Load current weather
+  selectedTime.textContent = "Now";
+  selectedConditions.src = weatherObject.current.condition.icon;
+  selectedTemperature.textContent = `${weatherObject.current.temp_f} F`;
+
+  // Load tomorrow's weather
+  const tomorrow = new Date(
+    `${weatherObject.forecast.forecastday[1].date}T00:00`
+  );
+  tomorrowDate.textContent = getWeekdayName(tomorrow.getDay());
+  tomorrowCondition.src =
+    weatherObject.forecast.forecastday[1].day.condition.icon;
+  tomorrowLow.textContent = `Low: ${weatherObject.forecast.forecastday[1].day.mintemp_f} F`;
+  tomorrowHigh.textContent = `High: ${weatherObject.forecast.forecastday[1].day.maxtemp_f} F`;
+
+  // Load the day after tomorrow's weather
+  const afterTomorrow = new Date(
+    `${weatherObject.forecast.forecastday[2].date}T00:00`
+  );
+  afterTomorrowDate.textContent = getWeekdayName(afterTomorrow.getDay());
+  afterTomorrowCondition.src =
+    weatherObject.forecast.forecastday[2].day.condition.icon;
+  afterTomorrowLow.textContent = `Low: ${weatherObject.forecast.forecastday[2].day.mintemp_f} F`;
+  afterTomorrowHigh.textContent = `High: ${weatherObject.forecast.forecastday[2].day.maxtemp_f} F`;
+};
+
+// Make an event listener for the search button
+searchButton.addEventListener("click", (e) => loadWeather(e));
+
+// Make event listeners for the hourly weather buttons
+for (let i = 0; i < hourButtons.length; i++) {
+  hourButtons[i].addEventListener("mouseover", (e) => {
+    selectedTime.textContent = e.target.id;
+    selectedConditions.src =
+      weatherObject.forecast.forecastday[0].hour[i].condition.icon;
+    selectedTemperature.textContent = `${weatherObject.forecast.forecastday[0].hour[i].temp_f} F`;
+  });
+}
+
+// driver script
+searchBar.value = "Baltimore, Maryland";
+loadWeather();
